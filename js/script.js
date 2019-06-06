69f1871ae8bb2c737ebf3eca62d733f8117640c3
artur <artur@mail.ru>
2019-06-06 11:44:45 +0300

    Added Json and responce
    Json and server responce

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-    json = $.parseJSON('[{"required": true, "fields": {"NAME": false}}, {"required": false, "fields": {"LAST_NAME": true}}, {"required": true, "fields": {"EMAIL": false, "PHONE": false}}, {"required": false, "fields": {"TYPE": false}}, {"required": false, "fields": {"TEXT": false}}]');
-    console.log(json);
-    console.log(json[0].fields);
-
     $('#submit').click(
         function() {
             formValidation();
@@ -18,25 +14,22 @@ $(document).ready(function() {
         about = $('#about').val();
         console.log(name, lastname, email, phone, about);
         json = $.parseJSON('[{"required": true, "fields": {"NAME": false}}, {"required": false, "fields": {"LAST_NAME": true}}, {"required": true, "fields": {"EMAIL": false, "PHONE": false}}, {"required": false, "fields": {"TYPE": false}}, {"required": false, "fields": {"TEXT": false}}]');
-        if (json[0].fields) {
-            if (name == "") { $('#name').css({ 'border': '1px solid red' }) }
+        if (json[0].fields.NAME || json[0].required) {
+            if (name == "") { 
+                $('#result_form').after('<p>Имя обязательное поле</p>'); 
+            }
         }
-        if (json[1].fields) {
+        if (json[1].fields.LAST_NAME || json[1].required) {
             if (lastname == "") {
-                $('#last-name').css({ 'border': '1px solid red' })
+                $('#result_form').after('<p>Фамилия обязательное поле</p>');
             }
         }
-        if (json[2].fields) {
-            if (email == "") {
-                $('#email').css({ 'border': '1px solid red' })
+        if (json[2].fields.EMAIL || json[2].fields.PHONE || json[2].required) {
+            if (email == "" && phone == "") {
+                $('#result_form').after('<p>Необходимо заполнить хотябы одно из полей "E-mail" или "Телефон"</p>');
             }
         }
-        if (json[3].fields) {
-            if (phone == "") {
-                $('#phone').css({ 'border': '1px solid red' })
-            }
-        }
-        if (json[4].fields) {
+        if (json[4].fields.TEXT) {
             if (about == "") {
                 $('#about').css({ 'border': '1px solid red' })
             }
@@ -47,14 +40,10 @@ $(document).ready(function() {
             dataType: "html",
             data: $("#form").serialize(),
             success: function(response) {
-                console.log(response);
                 result = $.parseJSON(response);
-                console.log(result);
                 $('#result_form').html('Имя: ' + result.success);
+                $('#result_form').html(result.data);
             },
-            error: function(response) { // Данные не отправлены
-                $('#result_form').html('Ошибка. Данные не отправлены.');
-            }
         });
     }
 });
