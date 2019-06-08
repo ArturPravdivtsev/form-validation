4d116f927d9c300f89a994cb1550a065b3461433
a.pravdivtcev <artiking@mail.ru>
2019-06-08 16:49:41 +0300

    Progress-bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
+    var numValid = 0;
+    var progress = $("#progress"),
+        progressMessage = $("#progress-message");
+    formProgess(numValid);
+    $("#form input").keyup(function() {
+        if ($(this).val().length == 1) {
+            numValid++;
+            formProgess(numValid);
+        }
+    });
+    $("#form textarea").keyup(function() {
+        if ($(this).val().length == 3) {
+            numValid++;
+            formProgess(numValid);
+        }
+    });
+    $("#form checkbox").mouseup(function() {
+        if ($(this + ':checked') == true) {
+            numValid++;
+            formProgess(numValid);
+        }
+    });
+
+
     var limit = 2;
     $('input.checkbox').on('change', function(evt) {
+        if ($('.do').find(':checked').length == 1) {
+            numValid++;
+            formProgess(numValid);
+        }
+        if ($('.do').find(':checked').length == 2) {
+            numValid++;
+            formProgess(numValid);
+        }
         if ($('.do').find(':checked').length > limit) {
             this.checked = false;
         }
@@ -26,7 +58,7 @@ $(document).ready(function() {
         json = $.parseJSON('[{"required": true, "fields": {"NAME": false}}, {"required": false, "fields": {"LAST_NAME": true}}, {"required": true, "fields": {"EMAIL": false, "PHONE": false}}, {"required": false, "fields": {"TYPE": false}}, {"required": false, "fields": {"TEXT": false}}]');
         if (json[0].fields.NAME || json[0].required) {
             if (name == "") {
-                $('#result_form').after('<p class="info">Имя обязательное поле</p>');
+                $('#result_form').after('<p class="info">Обязательное поле</p>');
             }
         }
         if (json[1].fields.LAST_NAME || json[1].required) {
@@ -54,5 +86,40 @@ $(document).ready(function() {
                 $('#result_form').html(result.data.message);
             },
         });
+    }
+
+    function formProgess(numValid) {
+        if (numValid == 0) {
+            progress.attr("value", "0");
+            progressMessage.text("0%");
+        }
+        if (numValid == 1) {
+            progress.attr("value", "14");
+            progressMessage.text("14%");
+        }
+        if (numValid == 2) {
+            progress.attr("value", "28");
+            progressMessage.text("28%");
+        }
+        if (numValid == 3) {
+            progress.attr("value", "42");
+            progressMessage.text("42%");
+        }
+        if (numValid == 4) {
+            progress.attr("value", "56");
+            progressMessage.text("56%");
+        }
+        if (numValid == 5) {
+            progress.attr("value", "70");
+            progressMessage.text("70");
+        }
+        if (numValid == 6) {
+            progress.attr("value", "84");
+            progressMessage.text("84");
+        }
+        if (numValid == 7) {
+            progress.attr("value", "100");
+            progressMessage.text("100%");
+        }
     }
 });
